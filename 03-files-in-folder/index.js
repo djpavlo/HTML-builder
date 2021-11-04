2809e9d60c0dbc94e47166cd8951b03d9f20f4cb
Pavlo <pavelmk8@gmail.com>
2021-11-04 18:27:56 +0300

    feat: implement 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+
+let folderPath = path.join(__dirname, '/secret-folder/');
+
+
+fs.promises.readdir(folderPath, { withFileTypes: true }).then(filenames => {
+  for (let fileData of filenames) {
+    if (!fileData.isDirectory()) {
+      let filePath = path.join(folderPath, fileData.name);
+      fs.stat(filePath, (err, stats) => {
+        let fileExt = path.extname(fileData.name);
+        console.log(`${fileData.name.replace(fileExt,'')}-${fileExt.replace('.','')}-${stats.size/1000}kb`);
+      });
+    }
+  }
+}).catch(err => {
+  console.log(err);
+});
